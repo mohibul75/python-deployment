4c0adff5f58cec31423b919d6bf5d582dec8cbc9
purbo75 <bsse1004@iit.du.ac.bd>
2023-02-27 02:02:27 +0600

    hello world api added

--- testProject/api/views.py
+++ testProject/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def helloWorld(request):
+    return Response('Hello world...')
